dce7f564d68fe598ca1a997493bd3c30373a010d
+ <+>
2022-02-24 13:52:09 -0800

    commit tree 2cadb74afec4632ed00e2f1c064c6436e6f48d93

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+//["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(arr) {
+  let thankYouArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    thankYouArr.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`);
+  }
+  return thankYouArr;
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"]));
+
+function countDown(num) {
+  let i = num;
+  while (i >= 0) {
+    console.log(i);
+    i--;
+  }
+}
+
+countDown(10);
